fe9009c355f29d8f3cad35b1149102650ecd273f
mic258147 <mic258147@gmail.com>
2018-10-25 23:06:47 +0800

    complete hw

---
+++ P18/source/Main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char string1[60] = "Welcome";
+	char string2[60];
+	int len;
+	strcpy_s(string2,60,string1);
+	printf("string2=%s\n", string2);
+	len = strlen(string2);
+	printf("¦r¦êªø«×¬°%d\n", len);
+	system("pause");
+	return 0;
+}
